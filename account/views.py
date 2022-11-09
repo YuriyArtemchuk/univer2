3612a8bd7c13bb521b4f0998383e3e79e00c7b5d
YuriyArtemchuk <uaa2uaa@gmail.com>
2022-11-09 21:37:29 +0200

    Add views and routes for account

--- account/views.py
+++ account/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
 
-def index(request):
+def reg(request):
+    return render(request, 'account/reg.html')
+
+
+def entry(request):
     return render(request, 'account/entry.html')
+
+
+def confirm(request):
+    return render(request, 'account/confirm.html')
+
+
+def exit(request):
+    return render(request, 'account/exit.html')
+
+
+def profile(request):
+    return render(request, 'account/profile.html')
+
+
+def reset(request):
+    return render(request, 'account/reset.html')
+
+
